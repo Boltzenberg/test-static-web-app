a5c577ed5204f05949fb138783404e99ff510e32
Boltzenberg <jon_rosenberg@hotmail.com>
2025-02-22 00:28:58 -0800

    Update to use cosmos DB

--- api/UpdateGroceryList.cs
+++ api/UpdateGroceryList.cs
@@ -2,6 +2,7 @@ using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Cosmos;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 using Boltzenberg.Functions.DataModels.GroceryList;
@@ -11,10 +12,60 @@ namespace Boltzenberg.Functions
     public class UpdateGroceryList
     {
         private readonly ILogger<UpdateGroceryList> _logger;
+        private const string EndpointUri = "https://gunga-test-cosmosdb.documents.azure.com:443/";
+        private static string PrimaryKey = Environment.GetEnvironmentVariable("GROCERY_LIST_PRIMARY_KEY");
+        private static CosmosClient cosmosClient = new CosmosClient(EndpointUri, PrimaryKey);
+        private static Database database = cosmosClient.GetDatabase("GungaDB");
+        private static Container container = database.GetContainer("GroceryList");
 
         public UpdateGroceryList(ILogger<UpdateGroceryList> logger)
         {
             _logger = logger;
+        }
+
+        private async Task<GroceryListDB> GetGroceryListFromCosmos(string listId)
+        {
+            string query = "SELECT * FROM c WHERE c.ListId = @listId";
+            QueryDefinition queryDefinition = new QueryDefinition(query).WithParameter("@listId", listId);
+            FeedIterator<GroceryListDB> queryResultSetIterator = container.GetItemQueryIterator<GroceryListDB>(queryDefinition);
+
+            GroceryListDB dataset = null;
+            if (queryResultSetIterator.HasMoreResults)
+            {
+                FeedResponse<GroceryListDB> currentResultSet = await queryResultSetIterator.ReadNextAsync();
+                dataset = currentResultSet.FirstOrDefault();
+            }
+
+            if (dataset == null)
+            {
+                dataset = new GroceryListDB();
+            }
+
+            return dataset;
+        }
+
+        private async Task<GroceryListDB> UpdateGroceryListToCosmos(GroceryListDB dataset)
+        {
+            try
+            {
+                ItemRequestOptions requestOptions = new ItemRequestOptions
+                {
+                    IfMatchEtag = dataset._etag
+                };
+
+                ItemResponse<GroceryListDB> updateResponse = await container.ReplaceItemAsync<GroceryListDB>(
+                    dataset,
+                    dataset.id,
+                    new PartitionKey(dataset.ListId),
+                    requestOptions);
+                return updateResponse.Resource;
+            }
+            catch (CosmosException ex) when (ex.StatusCode == System.Net.HttpStatusCode.PreconditionFailed)
+            {
+                // Handle the case where the ETag does not match
+            }
+
+            return null;
         }
 
         [Function("UpdateGroceryList")]
@@ -27,7 +78,7 @@ namespace Boltzenberg.Functions
                 return new BadRequestObjectResult("X-List-ID header is required");
             }
 
-            GroceryListDB dataset = GroceryListDB.GetCannedDB(listId);
+            GroceryListDB dataset = null;
 
             if (req.Method.ToLowerInvariant() == "post")
             {
@@ -37,21 +88,32 @@ namespace Boltzenberg.Functions
                     UpdateGroceryListPayload reqBody = JsonSerializer.Deserialize<UpdateGroceryListPayload>(body);
                     if (reqBody != null)
                     {
-                        foreach (GroceryListItem item in reqBody.ToRemove)
+                        do
                         {
-                            GroceryListItem itemToRemove = dataset.Items.Where(i => i.Item == item.Item).FirstOrDefault();
-                            if (itemToRemove != null)
-                            {
-                                dataset.Items.Remove(itemToRemove);
-                            }
-                        }
+                            dataset = await this.GetGroceryListFromCosmos(listId);
 
-                        foreach (GroceryListItem item in reqBody.ToAdd)
-                        {
-                            dataset.Items.Add(item);
-                        }
+                            foreach (GroceryListItem item in reqBody.ToRemove)
+                            {
+                                GroceryListItem itemToRemove = dataset.Items.Where(i => i.Item == item.Item).FirstOrDefault();
+                                if (itemToRemove != null)
+                                {
+                                    dataset.Items.Remove(itemToRemove);
+                                }
+                            }
+
+                            foreach (GroceryListItem item in reqBody.ToAdd)
+                            {
+                                dataset.Items.Add(item);
+                            }
+
+                            dataset = await this.UpdateGroceryListToCosmos(dataset);
+                        } while (dataset == null);
                     }
                 }
+            }
+            else
+            {
+                dataset = await this.GetGroceryListFromCosmos(listId);
             }
 
             string response = JsonSerializer.Serialize(dataset.Items);
--- api/datamodels/grocerylist/GroceryListDB.cs
+++ api/datamodels/grocerylist/GroceryListDB.cs
@@ -1,20 +1,33 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Boltzenberg.Functions.DataModels.GroceryList
 {
     public class GroceryListDB
     {
-        public string Id { get; set; }
+        public string ListId { get; set; }
         public List<GroceryListItem> Items { get; set; }
+        public string id { get; set; }
+        public string _etag { get; set; }
 
         public GroceryListDB()
         {
-            this.Id = string.Empty;
+            this.ListId = string.Empty;
             this.Items = new List<GroceryListItem>();
+            this.id = string.Empty;
+            this._etag = string.Empty;
+        }
+
+        public GroceryListDB(string listId)
+        {
+            this.ListId = listId;
+            this.Items = new List<GroceryListItem>();
+            this.id = string.Empty;
+            this._etag = string.Empty;
         }
 
         public static GroceryListDB GetCannedDB(string listId)
         {
-            GroceryListDB db = new GroceryListDB();
-            db.Id = listId;
+            GroceryListDB db = new GroceryListDB(listId);
             db.Items.Add(new GroceryListItem("Item 1"));
             db.Items.Add(new GroceryListItem("Item 2"));
             db.Items.Add(new GroceryListItem("Item 3"));
